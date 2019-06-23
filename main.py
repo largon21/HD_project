154714d766bba43add55b7eb2ae7ea8ccc9581f0
largon21 <34896467+largon21@users.noreply.github.com>
2019-06-23 11:12:36 +0200

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import GUI
 Application = GUI.App()
 
 Application.CreateWidget()
-# quote = """ble ble HAMLET: To be, or not to be--that is the question"""
+# quote = """ble ble HAMLET: To be, or not to be--that is the question ble ble ble"""
 # test.Insert_statement(quote)
 
 Application.Start_App()
