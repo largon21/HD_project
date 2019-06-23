a47e021acb359826325d43d02b56da0e19d25194
largon21 <34896467+largon21@users.noreply.github.com>
2019-06-23 11:42:59 +0200

    update

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ Application = GUI.App()
 Application.CreateWidget()
 # quote = """cytat:  'koniec'"""
 # test.Insert_statement(quote)
+# test.Insert_statement(quote)
 
 Application.Start_App()
+
+# test.Insert_statement(quote)
